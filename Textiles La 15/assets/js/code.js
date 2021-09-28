9b5cb56445eca343868fb75f38d668bdd5a6faf5
claudiaospina <claudiaospina@outlook.com>
2021-09-28 00:14:33 -0500

    Se agrega barra de opciones que incluye menú desplegable para el usuario.

---
+++ Textiles La 15/assets/js/code.js
@@ -0,0 +1,12 @@
+/* 
+alert("¡Bienvenido al Módulo de Ventas!")
+
+function cambiar(){
+    document.getElementById("titulo").innerHTML = "Prueba";
+}
+
+document.getElementById("usericon").onclick = function(){
+    cambiar();
+}
+
+ */
