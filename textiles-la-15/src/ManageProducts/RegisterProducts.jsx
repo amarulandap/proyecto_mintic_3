36754501b42602d63fb54b28fc8a03a85ac4a53e
RicardoRpo <ricar2011.medallo@gmail.com>
2021-10-18 18:29:07 -0500

    API funcionando con POSTMAN e imprimiendo los productos actuales Registerproducts

--- textiles-la-15/src/ManageProducts/RegisterProducts.jsx
+++ textiles-la-15/src/ManageProducts/RegisterProducts.jsx
@@ -1,20 +1,46 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./RegisterProductsStyles.css";
 
 
 function RegisterProducts(props) {
   /* let title = props.title; */
-  const [id, setId] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [precio, setPrecio] = useState("");
-  const [stock, setStock] = useState("");
-  const [fechaIngreso, setFechaIngreso] = useState("");
-  const [mRollos, setMRollos] = useState("");
+  const [precio, setPrecio] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [fechaIngreso, setFechaIngreso] = useState(new Date);
+  const [mRollos, setMRollos] = useState(0);
   const [datos, setDatos] = useState("");
+
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    try {
+        const response = await fetch("http://localhost:3001/get-products");
+        const jsonResponse = await response.json();
+        const responseProducts = jsonResponse.data;
+        const listProducts = responseProducts.map((product) =>
+            <tr>
+                <th scope="row">{product.Id}</th>
+                <td>{product.Descripcion}</td>
+                <td>{product.Precio}</td>
+                <td>{product.Stock}</td>
+                <td>{product.FechaIngreso}</td>
+                <td>{product.MRollos}</td>
+            </tr>
+        );
+        setProducts(listProducts)
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
+useEffect(()=>{
+  getProducts();
+}, []);
 
   const enviar = () => {
     let datosForm = {
-      'id':id,
       'descripcion': descripcion,
       'precio': precio,
       'stock': stock,
@@ -26,13 +52,14 @@ function RegisterProducts(props) {
     alert("Productos agregado correctamente")
   }
 
+
+
   return (
     <Fragment>
       <div className="container-fluid" id="form-product">
         <div className="row">
           <hr />
-          <div className="col-8 col-sm-8"></div>
-          <div className="col-4 col-sm-4">
+          <div className="container" className="col-4 col-sm-4 col-md-4 	col-lg-4 	col-xl-4 	col-xxl-4">
             <form className="container">
               <fieldset>
                 <center>
@@ -101,20 +128,7 @@ function RegisterProducts(props) {
                       type="text" 
                       id="metros" />
                     </div>
-                  </div>
-                  <br />
-                  <div className="row">
-                    <div className="col-sm-4">
-                      <label>Identificador</label>
-                    </div>
-                    <div className="col-sm-8">
-                      <input 
-                      onChange={(e) => setId(e.target.value)}
-                      type="text" 
-                      id="id" />
-                    </div>
-                  </div>
-                  <br />
+                  </div>            
                 </div>
                 <br />
                 
@@ -126,6 +140,23 @@ function RegisterProducts(props) {
                   />
               </fieldset>
             </form>
+          </div>
+          <div className="col-8 col-sm-8 col-md-8 	col-lg-8 	col-xl-8 	col-xxl-8">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Fecha Ingreso</th>
+                        <th scope="col">Metros por rollo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products}
+                </tbody>
+            </table> 
           </div>
         </div>
       </div>
