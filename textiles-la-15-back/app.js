0c3ae96f33cfa18cddd4b1f94759cd36d89dcf8a
amarulandap <88789392+amarulandap@users.noreply.github.com>
2021-10-26 19:43:02 -0500

    se actualiza la Api

--- textiles-la-15-back/app.js
+++ textiles-la-15-back/app.js
@@ -10,17 +10,36 @@ let connection;
 app.use(express.json());
 app.use(cors({origin: true}));
 
+/*Endpoint para obtener el listado de todos los usuarios*/
 app.get("/get-registers", async (request,response) => {
-    const [rows, fields] = await connection.execute("SELECT * FROM usuarios");
+    const [rows, fields] = await connection.execute("SELECT * FROM Usuarios");
     response.json({data: rows});
 })
 
+/*Endpoint para validar el rol del usuario y definir sus privilegios*/
 app.get("/get-register", async (request,response)=> {
     const email = request.query.email;
-    const [rows, fields] = await connection.execute(`SELECT * FROM usuarios WHERE Email='${email}'`);
+    const [rows, fields] = await connection.execute(`SELECT * FROM Usuarios WHERE Email='${email}'`);
     response.json(rows[0]);
 })
 
+/*Endpoint para obtener el listado de productos*/
+app.get("/get-products", async (request, response) => {
+    const [rows, fields] = await connection.execute("SELECT * FROM Productos");
+    console.log({ data: rows })
+    response.json({ data: rows });
+})
+
+/*Endpoint para busqueda de los productos*/
+app.get("/search-products", async (request,response)=> {
+    const busqueda = request.query.busqueda;
+    const valorBusqueda = request.query.valorBusqueda;
+    const [rows, fields] = await connection.execute(`SELECT * FROM Productos WHERE ${busqueda}='${valorBusqueda}'`);
+
+    response.json({data: rows});
+})
+
+/*Endpoint para ingresar la información de los usuarios al sistema*/
 app.post("/post-register", async (request,response) => {
     try{const {IDusuarios, PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, DireccionDomicilio, TelefonoFijo, TelefonoCelular, FechaIngreso, Email, Estado, Rol} = request.body;
     await connection.execute(`INSERT INTO Usuarios (IDusuarios, PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, DireccionDomicilio, TelefonoFijo, TelefonoCelular, FechaIngreso, Email, Estado, Rol) VALUES ('${IDusuarios}','${PrimerNombre}', '${SegundoNombre}', '${PrimerApellido}', '${SegundoApellido}', '${DireccionDomicilio}', '${TelefonoFijo}', '${TelefonoCelular}', '${FechaIngreso}', '${Email}', '${Estado}', '${Rol}')`);
@@ -31,18 +50,35 @@ app.post("/post-register", async (request,response) => {
     
 })
 
+/*Endpoint para agregar productos a la BBDD*/
+app.post("/add-product", async (req, res) => {
+    try {
+        console.log(req.body)
+        const {descripcion, precio, stock, fechaIngreso, mRollos} = req.body;
+        await connection.execute(`INSERT INTO Productos (Descripcion, Precio, Stock, FechaIngreso, MRollos) VALUES('${descripcion}', ${precio}, ${stock}, '${fechaIngreso}', ${mRollos})`);
+        res.json({status:"ok"})
+    }
+    catch (error) {
+        console.log(error);
+        res.json(error)
+    }
+    
+})
+
+/*Endpoint para actualizar el estado y el rol del usuario*/
 app.put("/put-register", async (request,response) => {
     try{const {IDusuarios, Estado, Rol} = request.body;
-    const [rows, fields] = await connection.execute(`UPDATE usuarios SET Estado='${Estado}', Rol='${Rol}' WHERE IDusuarios = '${IDusuarios}'`);
+    const [rows, fields] = await connection.execute(`UPDATE Usuarios SET Estado='${Estado}', Rol='${Rol}' WHERE IDusuarios = '${IDusuarios}'`);
     response.json({status: "ok"});
     }catch (error){ 
     response.json(error);
 }
 })
 
+/*Endpoint para eliminar usuarios*/
 app.delete("/delete-register", async (request,response) => {
     try{ const {IDusuarios} = request.body;
-    const [rows, fields] = await connection.execute('DELETE FROM usuarios WHERE IDusuarios = ?', [IDusuarios]);
+    const [rows, fields] = await connection.execute('DELETE FROM Usuarios WHERE IDusuarios = ?', [IDusuarios]);
     response.json({status: "ok"});
 }catch{
     response.json(error);
@@ -52,10 +88,11 @@ app.delete("/delete-register", async (request,response) => {
 
 app.listen(port, async () => {
     connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'pipe1981',
-    database: 'textiles_la_15',
+    host: 'sql10.freesqldatabase.com',
+    user: 'sql10446675',
+    password: 'DkeUw36e25',
+    database: 'sql10446675',
+    port: 3306,
     Promise: bluebird
   });
     console.log("El servidor esta escuchando")
